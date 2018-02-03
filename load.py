1eb3a78fd3dc2fae3a49040251164ac557f81c8e
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-03 22:50:13 +0100

    Add dataset loading

--- load.py
+++ load.py
@@ -12,3 +12,14 @@ from discover import divik_result_path, name_to_id
 
 Name = str
 Path = str
+
+
+@lru_cache(maxsize=10)
+def dataset(name: Name) -> spdata.types.Dataset:
+    """Load dataset by name
+
+    :param name: name of the dataset
+    :return: MSI data
+    """
+    with open(discover.data_path(name)) as data_file:
+        return spdata.reader.load_txt(data_file)
