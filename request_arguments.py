6ad65d5805b3ea20a4c1c5371c434a5a77333fbe
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-03 22:43:23 +0100

    Add required view argument definition

--- request_arguments.py
+++ request_arguments.py
@@ -18,3 +18,19 @@ def optional(variable_name, required_type):
     except TypeError:
         flask.abort(404)
     return variable
+
+
+def require(variable_name, required_type):
+    """Reads required variable from request arguments
+
+    If the argument has been provided with wrong type or has not been
+    provided at all, aborts with 404.
+
+    :param variable_name: name of the argument
+    :param required_type: expected argument type
+    :return: variable of expected type
+    """
+    variable = optional(variable_name, required_type)
+    if variable is None:
+        flask.abort(404)
+    return variable
