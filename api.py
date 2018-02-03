ba70e32105c06f0be5a85da1e6dc7a82a245d620
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-03 22:53:28 +0100

    Add conversion of NamedTuple to JSON

--- api.py
+++ api.py
@@ -8,3 +8,13 @@ import load
 from request_arguments import optional, require
 
 app = flask.Flask(__name__)
+
+
+def as_response(model: NamedTuple):
+    """Convert NamedTuple to Flask JSON response
+
+    :param model: returned data model
+    :return: JSON representation of data
+    """
+    # noinspection PyProtectedMember
+    return flask.jsonify(model._asdict())
