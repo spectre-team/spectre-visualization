673582b3ea51dd9d919a5284aeded6e445262a04
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-03 22:41:18 +0100

    Add DatasetMetadata model

--- models.py
+++ models.py
@@ -11,3 +11,16 @@ class CoordinatesRange(object):
         self.Min = int(vector.ravel().min())
         self.Max = int(vector.ravel().max())
 
+
+class DatasetMetadata(object):
+    def __init__(self, dataset_id: int, name: str, spectra_number: int,
+                 x_range: CoordinatesRange, y_range: CoordinatesRange):
+        """
+        :param name: name of dataset to extract metadata from
+        """
+        self.Id = dataset_id
+        self.Name = name
+        self.SpectraNumber = spectra_number
+        self.XRange = vars(x_range)
+        self.YRange = vars(y_range)
+
