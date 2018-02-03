f6c132f09afe86794a4fceba76b35d04068eb918
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-03 22:45:27 +0100

    Add name_to_id conversion

--- discover.py
+++ discover.py
@@ -7,3 +7,15 @@ _FILESYSTEM_ROOT = os.path.abspath(os.sep)
 DATA_ROOT = os.path.join(_FILESYSTEM_ROOT, 'data')
 Name = str
 Path = str
+
+
+@lru_cache(maxsize=1024)
+def name_to_id(entity_name: str) -> int:
+    """Get ID for a name
+
+    Computes SHA256 hash of name and converts it to int value.
+
+    :param entity_name: name of an object
+    :return: unique identifier
+    """
+    return int(hashlib.sha256(entity_name.encode()).hexdigest(), 16)
--- test/test_discover.py
+++ test/test_discover.py
@@ -5,3 +5,12 @@ import os
 import discover
 
 some_names = ["a", "blah", "wololo", "some_name", "other_name"]
+
+
+class TestNameToId(unittest.TestCase):
+    def test_generates_some_unique_ids(self):
+        ids = {discover.name_to_id(name) for name in some_names}
+        self.assertEqual(len(ids), len(some_names))
+
+    def test_id_is_consistent_for_the_same_name(self):
+        self.assertEqual(discover.name_to_id("blah"), discover.name_to_id("blah"))
