2bd2837a581ae27b7d26cb1918540f8da0ca5ed3
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-02-03 22:40:55 +0100

    Add CoordinatesRange model

--- models.py
+++ models.py
@@ -2,3 +2,12 @@ from typing import NamedTuple, List
 
 import numpy as np
 
+
+class CoordinatesRange(object):
+    def __init__(self, vector: np.ndarray):
+        """
+        :param vector: vector with locations across single coordinate
+        """
+        self.Min = int(vector.ravel().min())
+        self.Max = int(vector.ravel().max())
+
--- test/test_models.py
+++ test/test_models.py
@@ -4,3 +4,17 @@ import numpy as np
 
 import models
 
+
+class TestCoordinatesRange(unittest.TestCase):
+    def test_finds_bounds(self):
+        coordinates = np.array([1, 2, 6, 3, 4, 76, 2, -3])
+        _range = models.CoordinatesRange(coordinates)
+        self.assertEqual(_range.Min, -3)
+        self.assertEqual(_range.Max, 76)
+
+    def test_does_not_fail_for_strange_shape(self):
+        coordinates = np.array([[[1, 2, 6, 3, 4, 76, 2, -3]]])
+        _range = models.CoordinatesRange(coordinates)
+        self.assertEqual(_range.Min, -3)
+        self.assertEqual(_range.Max, 76)
+
